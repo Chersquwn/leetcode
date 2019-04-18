46ad624f126b183b42d511195b746dc6ff719ac1
Chersquwn <Chersquwn@163.com>
2019-04-18 22:04:54 +0800

    feat: add solution of 100th problem

---
+++ algorithms/Same Tree/index.js
@@ -0,0 +1,20 @@
+// problem https://leetcode.com/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+const isSameTree = function (p, q) {
+  if (!p && !q) return true
+  if (!p || !q || p.val !== q.val) return false
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
